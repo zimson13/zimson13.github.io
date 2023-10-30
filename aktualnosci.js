45cc30488a9842897a1ce735b1455af22c185897
zimson13 <146538901+zimson13@users.noreply.github.com>
2023-10-30 18:17:36 +0100

    Add files via upload

--- aktualnosci.js
+++ aktualnosci.js
@@ -1,17 +1,16 @@
 let container = document.querySelector(".container");
-
 let xhr = new XMLHttpRequest();
 xhr.open('GET', 'info.json', true);
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
         var content = xhr.responseText;
-        console.log(content);
         content = JSON.parse(content);
-        console.log(content);
         container.innerHTML = "";
         container.innerHTML += "I danie: " + content["1"] + "<br>";
         container.innerHTML += "II danie: " + content["2"] + "<br>";
         container.innerHTML += "Napój: " + content["napoj"] + "<br>";
+        container.innerHTML += "<hr>";
+        container.innerHTML += content["aktualnosci"];
     }
 };
 xhr.send();
