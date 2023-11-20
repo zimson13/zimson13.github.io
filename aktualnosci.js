719de8f68894afa44dc99df9a718acfd8fd34275
zimson13 <146538901+zimson13@users.noreply.github.com>
2023-11-20 13:01:33 +0100

    Add files via upload

--- aktualnosci.js
+++ aktualnosci.js
@@ -6,11 +6,16 @@ xhr.onreadystatechange = function() {
         var content = xhr.responseText;
         content = JSON.parse(content);
         container.innerHTML = "";
-        container.innerHTML += "I danie: " + content["1"] + "<br>";
-        container.innerHTML += "II danie: " + content["2"] + "<br>";
-        container.innerHTML += "Napój: " + content["napoj"] + "<br>";
-        container.innerHTML += "<hr>";
-        container.innerHTML += content["aktualnosci"];
+        container.innerHTML += "<h2>Obiad dnia </h2>";
+        container.innerHTML += "I danie: " + content["obiad-dnia"]["I-danie"] + "<br>";
+        container.innerHTML += "II danie: " + content["obiad-dnia"]["II-danie"] + "<br>";
+        container.innerHTML += "Napój: " + content["obiad-dnia"]["napoj"] + "<br>";
+        container.innerHTML += "<h2>Dzisiejsza oferta </h2>";
+        container.innerHTML += "Ciasta: " + content["dzisiejsza-oferta"]["ciasta"] + "<br>";
+        container.innerHTML += "Desery: " + content["dzisiejsza-oferta"]["desery"] + "<br>";
+        if(content["dzisiejsza-oferta"]["przekaski"] != "brak"){
+            container.innerHTML += "Przekaski: " + content["dzisiejsza-oferta"]["przekaski"] + "<br>";
+        }
     }
 };
 xhr.send();
